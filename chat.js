978f7118e4cf602a9ee1eefce538d1cbfcb4aa0e
krishna kireeti <kirikom94@gmail.com>
2023-08-30 22:09:47 +0530

    chat app changes

---
+++ chat.js
@@ -0,0 +1,44 @@
+const socket = io();
+let username;
+
+do {
+    username = prompt("Please enter your username:");
+} while (!username);
+
+socket.emit("user joined", username);
+
+socket.on("update userList", (users) => {
+  const contactsList = document.querySelector(".contact-list");
+  contactsList.innerHTML = ""; // Clear the list first
+
+  users.forEach((user) => {
+    const contactItem = document.createElement("li");
+    contactItem.className = "contact";
+
+    const contactNameSpan = document.createElement("span");
+    contactNameSpan.className = "contact-name";
+    contactNameSpan.textContent = user.name;
+
+    contactItem.appendChild(contactNameSpan);
+    contactsList.appendChild(contactItem);
+  });
+
+});
+
+document.getElementById('sendButton').addEventListener('click', () => {
+    const messageInput = document.getElementById('messageInput')
+    const message = messageInput.value;
+    if (message.trim() !== '') {
+        socket.emit('chat message', message);
+        messageInput.value = '';
+    }
+});
+
+socket.on('chat message', (data) => {
+    const messagesDiv = document.querySelector(".messages");
+    const newMessageDiv = document.createElement("div");
+    newMessageDiv.className = "message received";
+    newMessageDiv.textContent = data;
+    messagesDiv.appendChild(newMessageDiv);
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+});
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+let onlineUsers = [];
+
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
+
+  socket.on('user joined', (username) => {
+    if(onlineUsers.indexOf(username) === -1) {
+      onlineUsers.push(username);
+      socket.username = username;
+      io.emit('update userlist', onlineUsers);
+    }
+  })
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg); // Send the message to all connected clients
